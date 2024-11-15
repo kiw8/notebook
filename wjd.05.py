10a88b15d1f1d1a0d0f63ad86b8afa22502c85e9
jw.08 <dolmenari4@naver.com>
2024-11-15 14:12:17 +0900

    jw.08

---
+++ wjd.05.py
@@ -0,0 +1,6 @@
+from random import *
+
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
