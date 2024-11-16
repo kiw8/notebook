80a08aee20788d7f0761393764fbef87c385401e
jw.08 <dolmenari4@naver.com>
2024-11-16 19:57:23 +0900

    jw.08

---
+++ MLP 모델설계.py
@@ -0,0 +1,129 @@
+﻿import numpy as np  # 선형대수 모듈
+import matplotlib.pyplot as plt  # 시각화 모듈
+import torch  # 파이토치
+import torch.nn as nn  # Pytorch의 모듈을 모아놓은 것. from~이 아닌 저렇게 임포트를 하는것이 거의 관습이라고 함
+import torch.nn.functional as F  # toch.nn 중에서 자주 쓰는 함수를 F로 임포트
+import torch.nn.init as init  # 초기화 관련 모둘
+import torchvision  # TorchVision 임포트
+from torchvision import transforms, datasets # 데이터를 다루기 위한 TorchVision 내의 Transforms와 datasets를 따로 임포트
+
+DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+BATCH_SIZE = 32  # 한번에 학습을 실행할 떄 사용할 데이터의 크기를 정함
+EPOCHS = 30 # 전체 데이터를 한 번 학습 돌리는 것이 1EPOCH이므로 여기에서는 30번 학습 돌리는것을 의미함
+
+print('Using PyTorch version:', torch.__version__, 'Device:', DEVICE)
+
+train_dataset = datasets.MNIST(
+    root="../data/MNIST",
+    train=True,
+    download=True,
+    transform=transforms.ToTensor()
+)
+test_dataset = datasets.MNIST(
+    root="../data/MNIST",
+    train=False,
+    download=True,
+    transform=transforms.ToTensor()
+)
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+
+) #DataLoader는 위에서 불러온 dataset을 사전에 설정한 batch size(32)로 미니 배치 설정을 할수 있게 도움
+
+for (X_train, y_train) in train_loader:
+    print('X_train:', X_train.size(), 'type:', X_train.type())
+    print('y_train:', y_train.size(), 'type:', y_train.type())
+    break
+
+def imshow(img):
+    # img = img / 2 + 0.5     # denormalize
+    npimg = img.numpy()
+    plt.axis('off')
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(train_loader)  # iterator
+images, labels = next(dataiter)  # next() 함수 사용
+
+
+imshow(torchvision.utils.make_grid(images))
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+        self.dropout_prob = 0.5 # 50%의 노드에 대한 가중치 계산을 하지 않기 위한 설정
+        self.batch_norm1 = nn.BatchNorm1d(512) # 1dimension이기 때문에 BatchNorm1d를 사용함.
+        self.batch_norm2 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.fc1(x)
+        x = self.batch_norm1(x)
+        x = F.relu(x) # sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x)
+        x = self.batch_norm2(x)
+        x = F.relu(x) # sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            init.kaiming_uniform_(m.weight.data)
+
+model = MLP().to(DEVICE)  # 정의한 모델을 GPU로 납치
+model.apply(MLP.weight_init)
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+loss_fn = nn.CrossEntropyLoss()
+
+# print(model)
+def train(model, train_loader, optimizer, log_interval):
+    model.train()
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = loss_fn(output, label)
+        loss.backward()
+        optimizer.step()
+
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += loss_fn(output, label).item()
+            prediction = output.max(1, keepdim=True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+for Epoch in range(1, EPOCHS + 1):
+    train(model, train_loader, optimizer, log_interval=100)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print("[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} %".format(
+        Epoch, test_loss, test_accuracy
+    ))
