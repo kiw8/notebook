3a00b27616894c498dc9aac889e512d2ac25e2c2
jw.08 <dolmenari4@naver.com>
2024-11-16 20:04:10 +0900

    jw.08

--- wjd.05.py
+++ wjd.05.py
@@ -56,3 +56,74 @@ images, labels = next(dataiter)  # next() 함수 사용
 
 imshow(torchvision.utils.make_grid(images))
 
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+        self.dropout_prob = 0.5 # 50%의 노드에 대한 가중치 계산을 하지 않기 위한 설정
+        self.batch_norm1 = nn.BatchNorm1d(512) # 1dimension이기 때문에 BatchNorm1d를 사용함.
+        self.batch_norm2 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.fc1(x)
+        x = self.batch_norm1(x)
+        x = F.relu(x) # sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x)
+        x = self.batch_norm2(x)
+        x = F.relu(x) # sigmoid(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            init.kaiming_uniform_(m.weight.data)
+
+model = MLP().to(DEVICE)  # 정의한 모델을 GPU로 납치
+model.apply(MLP.weight_init)
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+loss_fn = nn.CrossEntropyLoss()
+
+# print(model)
+def train(model, train_loader, optimizer, log_interval):
+    model.train()
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = loss_fn(output, label)
+        loss.backward()
+        optimizer.step()
+
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += loss_fn(output, label).item()
+            prediction = output.max(1, keepdim=True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+for Epoch in range(1, EPOCHS + 1):
+    train(model, train_loader, optimizer, log_interval=100)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print("[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} %".format(
+        Epoch, test_loss, test_accuracy
+    ))
